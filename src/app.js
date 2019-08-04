ddb6b265c37a0c43fca9e91e7a49a357ef8dace2
Soulyana <soulyana.lakew@accenturefederal.com>
2019-08-04 03:56:01 -0400

    fixed sp bug

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
 const accounts = JSON.parse(accountData);
 
-const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.join'), 'utf8');
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
 const users = JSON.parse(userData);
 
 app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts}));
